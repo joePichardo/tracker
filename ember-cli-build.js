9e9da0762872ec4e0114ec0c972aafa653b0a14d
joePichardo <joepichar@gmail.com>
2016-09-24 08:38:12 -0700

    first commit base of app, with jqyery and bootstrap

---
+++ ember-cli-build.js
@@ -0,0 +1,37 @@
+/*jshint node:true*/
+/* global require, module */
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+var bootstrapPath = 'bower_components/bootstrap-sass/assets/';
+
+module.exports = function(defaults) {
+  var app = new EmberApp(defaults, {
+    // Add options here
+    sassOptions: {
+      includePaths: [
+        bootstrapPath + 'stylesheets'
+      ]
+    }
+  });
+
+  // Use `app.import` to add additional libraries to the generated
+  // output files.
+  //
+  // If you need to use different assets in different
+  // environments, specify an object as the first parameter. That
+  // object's keys should be the environment name and the values
+  // should be the asset to use in that environment.
+  //
+  // If the library that you are including contains AMD or ES6
+  // modules that you would like to import into your application
+  // please specify an object with the list of modules as keys
+  // along with the exports of each module as its value.
+
+  // ... Template comments ...
+
+  //Create paths to bootstrap assets
+  // Add assets to app with import
+  app.import(bootstrapPath + 'javascripts/bootstrap.js')
+
+  return app.toTree();
+};
