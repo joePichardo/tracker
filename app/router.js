e0b6caa30d52147b8b6a6ae64a11047adf193dac
joePichardo <joepichar@gmail.com>
2016-09-26 20:17:54 -0700

    templates for pages, each grab their own route with data attached

--- app/router.js
+++ app/router.js
@@ -12,10 +12,10 @@ Router.map(function() {
     this.route('new');
   });
   this.route('sighting', function() {
-    this.route('edit');
+    this.route('edit', {path: "sightings/:sighting_id/edit"});
   });
   this.route('cryptids');
-  this.route('cryptid');
+  this.route('cryptid', {path: 'cryptids/:cryptid_id'});
   this.route('witnesses');
   this.route('witness');
 });
