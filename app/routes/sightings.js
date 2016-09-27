f6c0ddad5b0c1bbefd81103cadf1db72a813aa64
joePichardo <joepichar@gmail.com>
2016-09-26 20:16:07 -0700

    route function to show all sightings

--- app/routes/sightings.js
+++ app/routes/sightings.js
@@ -3,23 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model(){
-    let record1 = this.store.createRecord('sighting', {
-      location: 'Atlanta',
-      sightedAt: new Date('2016-02-09')
-    });
-    let record2 = this.store.createRecord('sighting', {
-      location: 'Calloway',
-      sightedAt: new Date('2016-03-14')
-    });
-    let record3 = this.store.createRecord('sighting', {
-      location: '',
-      sightedAt: new Date('2016-03-21')
-    });
-
-    // record1.set('location', 'Paris, France');
-    console.log("Record 1 location: " + record1.get('location'));
-
-    return [record1, record2,  record3];
+    return this.store.findAll('sighting', {reload: true});
   }
 
 });
